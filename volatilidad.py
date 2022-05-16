b0a961d004302ae040f6a479029825e6fbf23029
Efren Sandoval <efren@efrensandoval.dev>
2022-05-16 15:39:27 -0500

    normalized data

---
+++ volatilidad.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+
+saldos_cuentas_depositos_original_df = pd.read_excel("./Downloads/wilmer/insumos/volatilidad.xls", usecols="B:F")
+scdc1_df = saldos_cuentas_depositos_original_df.iloc[0:60].reset_index(drop=True)
+scdc2_df = saldos_cuentas_depositos_original_df.iloc[30:90].reset_index(drop=True)
+
+scdr_df = np.log(scdc2_df.divide(scdc1_df)).fillna(0)
+
+print(scdr_df)
