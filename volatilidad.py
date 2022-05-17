7559071d6981933b2df0fa0990602ca1e0430565
Efren Sandoval <efren@efrensandoval.dev>
2022-05-17 15:05:34 -0500

    correlation matrix, var and vart calculated

--- volatilidad.py
+++ volatilidad.py
@@ -6,6 +6,12 @@ saldos_depositos_df = pd.read_excel("volatilidad.xls", usecols = "B:F")
 saldos_depositos_1 = saldos_depositos_df.iloc[0:60].reset_index(drop=True)
 saldos_depositos_2 = saldos_depositos_df.iloc[30:90].reset_index(drop=True)
 
-var_s = np.log(saldos_depositos_2.divide(saldos_depositos_1)).fillna(0).std(axis=0)
+saldos_normalizados = np.log(saldos_depositos_2.divide(saldos_depositos_1)).fillna(0)
 
-print(var_s)
+var_s = saldos_normalizados.std(axis=0).multiply(saldos_depositos_df.iloc[89])
+
+corr_matrix = saldos_normalizados.corr().fillna(0)
+
+var_total = np.sqrt(var_s.dot(var_s.transpose().dot(corr_matrix)))
+
+
