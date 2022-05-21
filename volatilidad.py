642c421e44aaef9618f99d02a55206b3564cb0f1
Efren Sandoval <efren@efrensandoval.dev>
2022-05-21 10:29:58 -0500

    volatilidad calculada respecto del saldo total diario para los últimos 5 días

--- volatilidad.py
+++ volatilidad.py
@@ -9,17 +9,21 @@ def calcular_volatilidad_general(saldos_df):
 
   saldos_normalizados = np.log(saldos_2.divide(saldos_1)).fillna(0)
 
-  var_s = saldos_normalizados.std(axis=0).multiply(saldos_df.iloc[89])
-
   corr_matrix = saldos_normalizados.corr().fillna(0)
 
+  var_s = saldos_normalizados.std(axis=0).multiply(saldos_df.iloc[89])
+
   var_total = np.sqrt(var_s.dot(var_s.transpose().dot(corr_matrix)))
+
+  total_depositos_last_week = saldos_df.iloc[85:90].agg('sum', axis=1)
+
+  volatilidad_per_day = var_total/total_depositos_last_week
 
   total_depositos_last_day = saldos_df.iloc[89].agg('sum')
 
   volatilidad_general = var_total/total_depositos_last_day
 
-  return volatilidad_general
+  return volatilidad_per_day
 
 
 print(calcular_volatilidad_general(saldos_depositos_df))
